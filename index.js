f4c6344ac44efb5e69761ee162301f89f3b0823c
109062126 <ssuyung@gapp.nthu.edu.tw>
2022-04-18 13:54:02 +0800

    finished sign in with Google

--- index.js
+++ index.js
@@ -34,6 +34,19 @@ export class Example extends React.Component {
     }
     signInWithGoogle(){
         console.log("Siginin in with Google");
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            var token = result.credential.accessToken;
+            var user = result.user;
+            console.log("User "+user.email+" is signed in with Google");
+        })
+        .catch(function(error){
+            var errorCode = error.code;
+            var errorMessage = error.Message;
+            var email = error.email;
+            var credential = error.credential;
+            console.log(errorMessage);
+        })
     }
     render(){
         return(
