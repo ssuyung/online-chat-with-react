30e3ac71cc540961d8964de8cc887454c63da25e
109062126 <ssuyung@gapp.nthu.edu.tw>
2022-04-18 12:42:47 +0800

    finished signin page

--- index.js
+++ index.js
@@ -2,7 +2,48 @@ import React from "react";
 
 export class Example extends React.Component {
     render(){
-        return(<div><h1>Hello World!</h1></div>);
+        return(
+            <div class="text-center">
+                <h1>Hello World!</h1>
+                {/* <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+                    <a class="navbar-brand" href="#">Simple Forum</a>
+                    <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
+                                <div id="dynamic-menu" class="dropdown-menu" aria-labelledby="dropdown01">
+                                    <a class="dropdown-item" href="signin.html">Login</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav> */}
+                
+                <div className="form-signin">
+                    <img className="mb-4" src="img/pic2-01.png" alt="" height="108"></img>
+                    <h1 className="h3 mb-3 font-weight-normal">Please sign in/register</h1>
+                    <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus></input>
+                    <label htmlFor="inputPassword" className="sr-only">Password</label>
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required></input>
+                    <div className="checkbox mb-3">
+                    
+                    <input type="checkbox" id="remember" value="remember-me"></input>
+                    <label htmlFor="remember">remember me</label>
+                    
+                    </div>
+                    <button className="btn btn-lg btn-primary btn-block" id="btnLogin">Sign in</button>
+                    <button className="btn btn-lg btn-info btn-block" id="btngoogle">Sign in with Google</button>
+                    <button className="btn btn-lg btn-secondary btn-block" id="btnSignUp">New account</button>
+                </div>
+
+
+            </div>
+        );
     }
 }
 
---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyDucltzrPyia36VHJ2Yv1XiX_j2bHsJoCo",
+    authDomain: "chatroom-687cb.firebaseapp.com",
+    databaseURL: "https://chatroom-687cb-default-rtdb.firebaseio.com",
+    projectId: "chatroom-687cb",
+    storageBucket: "chatroom-687cb.appspot.com",
+    messagingSenderId: "481594375827",
+    appId: "1:481594375827:web:b2b08298fd3b49cb9a1766"
+  };
+  firebase.initializeApp(config);
