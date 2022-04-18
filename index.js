74390dbab0658c53de5c55ab47bc47b280154a60
109062126 <ssuyung@gapp.nthu.edu.tw>
2022-04-18 13:49:17 +0800

    finished signin and signup

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 export class Example extends React.Component {
+    signIn(){
+        console.log("signing in");
+        var email = document.getElementById("inputEmail").value,
+            password = document.getElementById("inputPassword").value;
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) =>{
+            var user = userCredential.user;
+            console.log("User "+ user.email + " is signed in");
+        })
+        .catch((error) =>{
+            var errorCode = error.code;
+            console.log(error.message);
+        })
+
+    }
+    signUp(){
+        console.log("Signing up");
+        var email = document.getElementById("inputEmail").value,
+            password = document.getElementById("inputPassword").value;
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential)=>{
+            var user = userCredential.user;
+            // console.log(user);
+            // console.log(user.email);
+            console.log("User "+user.email+" is signed up");
+        })
+        .catch((error)=>{
+            var errorCode = error.code;
+            console.log(error.message);
+        });
+    }
+    signInWithGoogle(){
+        console.log("Siginin in with Google");
+    }
     render(){
         return(
-            <div class="text-center">
-                <h1>Hello World!</h1>
+            <div className="text-center">
+                <h1>Hello there!</h1>
                 {/* <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
                     <a class="navbar-brand" href="#">Simple Forum</a>
                     <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
@@ -24,21 +58,21 @@ export class Example extends React.Component {
                 </nav> */}
                 
                 <div className="form-signin">
-                    <img className="mb-4" src="img/pic2-01.png" alt="" height="108"></img>
+                    <img className="mb-4" src="./src/img/pic2-01.png" alt="" height="108"></img>
                     <h1 className="h3 mb-3 font-weight-normal">Please sign in/register</h1>
                     <label htmlFor="inputEmail" className="sr-only">Email address</label>
                     <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus></input>
                     <label htmlFor="inputPassword" className="sr-only">Password</label>
                     <input type="password" id="inputPassword" className="form-control" placeholder="Password" required></input>
                     <div className="checkbox mb-3">
-                    
-                    <input type="checkbox" id="remember" value="remember-me"></input>
-                    <label htmlFor="remember">remember me</label>
+                        
+                        <input type="checkbox" id="remember" value="remember-me"></input>
+                        <label htmlFor="remember">remember me</label>
                     
                     </div>
-                    <button className="btn btn-lg btn-primary btn-block" id="btnLogin">Sign in</button>
-                    <button className="btn btn-lg btn-info btn-block" id="btngoogle">Sign in with Google</button>
-                    <button className="btn btn-lg btn-secondary btn-block" id="btnSignUp">New account</button>
+                    <button className="btn btn-lg btn-primary btn-block" id="btnLogin" onClick={()=>this.signIn()}>Sign in</button>
+                    <button className="btn btn-lg btn-info btn-block" id="btngoogle" onClick={()=>this.signInWithGoogle()}>Sign in with Google</button>
+                    <button className="btn btn-lg btn-secondary btn-block" id="btnSignUp" onClick={()=>this.signUp()}>New account</button>
                 </div>
 
 
