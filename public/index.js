6910c061cfb9238b08e35a3df2450a549e3efb67
109062126 <ssuyung@gapp.nthu.edu.tw>
2022-04-22 00:37:36 +0800

    updated public directory files

---
+++ public/index.js
@@ -0,0 +1,18 @@
+import React, { useReducer } from "react";
+import { BrowserRouter, Route, Routes, HashRouter} from "react-router-dom";
+// import {hashHistory} from "react-router";
+import {Login} from "./src/component/Login.js";
+import {Chatroom} from "./src/component/Chatroom.js";
+
+
+
+ReactDOM.render((
+    <HashRouter>
+        <Routes>
+            <Route path="/" element={<Login/>}></Route>
+            <Route path="chatroom" element={(<Chatroom />)}></Route>
+        </Routes>
+    </HashRouter>
+),document.getElementById("example"));
+// ReactDOM.render(<Example/>, document.getElementById("example"));
+// console.log(document.getElementById("example").isLoggedIn);
